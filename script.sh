cc6b92ee0a8fe3f402660f3628b8d0b3a1ecd2e7
Altay <36895904+oguzbeyaz06@users.noreply.github.com>
2022-06-15 15:19:26 +0300

    Create script.sh

---
+++ script.sh
@@ -0,0 +1,46 @@
+#!/bin/bash
+
+
+
+echo "*********************"
+echo "Massa - Create a Node"
+echo "*********************"
+sleep 5
+
+sudo apt update
+sudo ufw enable
+sudo ufw allow 22
+sudo ufw allow ssh
+sudo ufw allow 31244/tcp
+sudo ufw allow 31245/tcp
+sudo ufw status
+
+echo "---------------------"
+echo "--------------------- Portlar ayarlandı"
+echo "--------------------- Düğüm yükleniyor..."
+echo "---------------------"
+sleep 5
+
+sudo apt install pkg-config curl git build-essential libssl-dev libclang-dev
+curl --proto '=https' --tlsv1.2 -sSf https://sh.rustup.rs | sh
+source $HOME/.cargo/env
+rustc --version
+rustup toolchain install nightly
+rustup default nightly
+rustup default nightly
+rustc --version
+git clone --branch testnet https://github.com/massalabs/massa.git
+
+echo "---------------------"
+echo "Düğüm yüklendi... dosyalar oluşturuluyor lütfen bekleyin"
+echo "---------------------"
+sleep 5
+echo "------------------------------"
+echo "------------------------------"
+echo " Sunucu ip adresini giriniz :"
+read ipadr
+echo -e "[network] \nroutable_ip = '$ipadr' \n" >> cd/massa/massa-node/config/config.toml
+
+echo "Routable dosyası oluşturuldu, sunucu yeniden başlatılıyor..."
+sleep 3
+reboot
