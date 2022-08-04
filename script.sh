a51a5d136cbab712f0f8326efa4e32f5fc5fdf3b
Oğuzhan <36895904+altai0@users.noreply.github.com>
2022-08-04 21:03:47 +0300

    Update script.sh

--- script.sh
+++ script.sh
@@ -1,11 +1,19 @@
 #!/bin/bash
 
 
+bold=$(tput bold)
+underline=$(tput smul)
+italic=$(tput sitm)
+info=$(tput setaf 2)
+error=$(tput setaf 160)
+warn=$(tput setaf 214)
+reset=$(tput sgr0)
 
+clear;
 echo "*********************---*********************"
 echo "Massa - The decentralized and scaled blockchain"
 echo "*********************---*********************"
-sleep 5
+sleep 4
 
 # server update and port settings
 sudo apt-get update -y
@@ -17,11 +25,10 @@ sudo ufw allow 31244/tcp
 sudo ufw allow 31245/tcp
 sudo ufw status
 
-echo "---------------------"
-echo "--------------------- Portlar ayarlandı düğüm yükleniyor..."
-echo "--------------------- Node loading please wait"
-echo "---------------------"
-sleep 3
+clear;
+echo "${info}INFO${reset}: installation is ${bold}set${reset} please wait... "
+
+sleep 5
 
 # required libraries
 sudo apt install pkg-config curl git build-essential libssl-dev libclang-dev -y
@@ -39,19 +46,18 @@ rustup default nightly
 rustup update
 
 # settings file
+clear;
 echo "---------------------"
-echo "Düğüm başarıyla yüklendi..."
-echo "Node installed successfully..."
+echo "${info}INFO${reset}: Node installed ${bold}successfully${reset}..."
 echo "---------------------"
 sleep 2
 
-echo " Sunucu ip adresini giriniz (Enter your server's ip address) :"
+echo "Enter your server's ip address :"
 read ipadr
 echo -e "[network]\nroutable_ip = '$ipadr'" >> massa/massa-node/config/config.toml
 #echo -e '[network]\nroutable_ip = "$ipadr"' >> massa/massa-node/config/config.toml
 
 # reboot to take effect
-echo "Restarting the server for the settings to take effect."
-echo "Routable dosyası oluşturuldu, ayarların geçerli olabilmesi için sunucu yeniden başlatılıyor..."
-sleep 2
+echo "${info}INFO${reset}: Restarting the server for the settings to take ${bold}effect${reset}..."
+sleep 5
 reboot
