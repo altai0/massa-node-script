a0bd87ce3c921630014f40b1e067c3647cf3ec8d
Oğuzhan <36895904+altai0@users.noreply.github.com>
2023-02-26 15:25:23 +0300

    Latest Docker

--- docker-installation.sh
+++ docker-installation.sh
@@ -2,21 +2,26 @@ clear;
 echo "Altay - Massa Node Installation (Docker)"
 sleep 3
 
-### Docker and docker compose prerequisites
-sudo apt install curl
-sudo apt install gnupg
-sudo apt install ca-certificates
-sudo apt install lsb-release
+#uninstall old version docker
+sudo apt-get remove docker docker-engine docker.io containerd runc
 
-### Download the docker gpg file to Ubuntu
-sudo mkdir -p /etc/apt/keyrings
+#install
+sudo apt-get update
+sudo apt-get install \
+    ca-certificates \
+    curl \
+    gnupg \
+    lsb-release
+sudo mkdir -m 0755 -p /etc/apt/keyrings
 curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo gpg --dearmor -o /etc/apt/keyrings/docker.gpg
+echo \
+  "deb [arch=$(dpkg --print-architecture) signed-by=/etc/apt/keyrings/docker.gpg] https://download.docker.com/linux/ubuntu \
+  $(lsb_release -cs) stable" | sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
 
-### Add Docker and docker compose support to the Ubuntu's packages list
-echo "deb [arch=$(dpkg --print-architecture) signed-by=/etc/apt/keyrings/docker.gpg] https://download.docker.com/sudo apt-get install docker-ce docker-ce-cli containerd.io docker-compose-pluginsudo apt-get install docker-ce docker-ce-cli containerd.io docker-compose-pluginlinux/ubuntu   $(lsb_release -cs) stable" | sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
+sudo chmod a+r /etc/apt/keyrings/docker.gpg  
+sudo apt-get update
+sudo apt-get install docker-ce docker-ce-cli containerd.io docker-buildx-plugin docker-compose-plugin
 
-sudo apt update
-sudo apt install docker-ce docker-ce-cli containerd.io docker-compose-plugin
 
 #created docker-compose file
 cat <<EOF >docker-compose.yml
