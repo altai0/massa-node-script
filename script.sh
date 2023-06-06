3dda989a5be7248465edbc90c02792cc263b4bea
Oğuzhan <36895904+altai0@users.noreply.github.com>
2023-06-06 22:22:05 +0300

    Update script.sh

--- script.sh
+++ script.sh
@@ -40,7 +40,7 @@ rustc --version
 
 # From binaries
 wget https://github.com/massalabs/massa/releases/download/TEST.23.0/massa_TEST.23.0_release_linux.tar.gz
-tar -xf massa_TEST.22.2_release_linux.tar.gz
+tar -xf massa_TEST.23.0_release_linux.tar.gz
 
 # settings file
 clear;
