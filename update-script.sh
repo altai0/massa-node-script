150aa0b614c499827496a4371a791c033f3e9bf0
Oğuzhan <36895904+altai0@users.noreply.github.com>
2023-06-11 01:46:26 +0300

    Update update-script.sh

--- update-script.sh
+++ update-script.sh
@@ -3,21 +3,12 @@
 #delete old version
 sudo rm script.sh
 sudo rm script.sh.1
-sudo rm -r newVersion
-sudo rm massa/massa-node/massa-node
-sudo rm massa/massa-client/massa-client
 
 # fetch new version 
 
-mkdir newVersion
 wget https://github.com/massalabs/massa/releases/download/TEST.23.1/massa_TEST.23.1_release_linux.tar.gz
-tar -zxf massa_TEST.23.1_release_linux.tar.gz --directory /newVersion
+tar -zxf massa_TEST.23.1_release_linux.tar.gz
 
-
-# update node
-
-mv newVersion/massa/massa-node/massa-node massa/massa-node/
-mv newVersion/massa/massa-client/massa-client massa/massa-client/
 
 echo "--------"
 echo "Done..."
