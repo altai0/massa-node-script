13bcc4d7760e5741f4d9028c27d7de9681c7be87
Oğuzhan <36895904+altai0@users.noreply.github.com>
2023-06-11 01:53:57 +0300

    Update update-script.sh

--- update-script.sh
+++ update-script.sh
@@ -1,15 +1,9 @@
 # UPDATE
-
-#delete old version
-sudo rm script.sh
-sudo rm script.sh.1
-
-# fetch new version 
-
-wget https://github.com/massalabs/massa/releases/download/TEST.23.1/massa_TEST.23.1_release_linux.tar.gz
-tar -zxf massa_TEST.23.1_release_linux.tar.gz
-
-
-echo "--------"
-echo "Done..."
-echo "--------"
+while true; do
+    read -p "Massa node will be completely deleted and reloaded (this may mean losing your addresses, please do not start this without saving your address) " yn
+    case $yn in
+        [Yy]* ) sudo rm script.sh; sudo rm -r massa; wget https://github.com/massalabs/massa/releases/download/TEST.23.1/massa_TEST.23.1_release_linux.tar.gz; tar -zxf massa_TEST.23.1_release_linux.tar.gz; echo "Done...";
+        [Nn]* ) exit;;
+        * ) echo "Please answer yes or no.";;
+    esac
+done
